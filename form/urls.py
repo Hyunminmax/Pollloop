250f16b8f8a5dd6355b0aad79ed476be5b7b9768
Hyunminmax <hyunmin.shin81@gmail.com>
2025-01-08 13:25:46 +0900

    feat/for006 - 폼 제출 대상중 : 제출 여부를 보여주는 값 반환

--- form/urls.py
+++ form/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import FormCreateView, FormListView, FormView, FormInvitedView, FormSubmitView, FromSummaryView, FromDataView
+from .views import (
+    FormCreateView, FormListView, FormView, 
+    FormInvitedView, FormSubmitView, FromSummaryView, 
+    FromDataView, FormCompletedUserView
+)
 
 urlpatterns = [
     path('uuid:<slug:uuid>/', FormView.as_view(), name='FormLoad'),
@@ -7,6 +11,7 @@ urlpatterns = [
     path('invited/', FormInvitedView.as_view(), name='FormInvited'),
     path('summary/uuid:<slug:uuid>', FromSummaryView.as_view(), name='FormSummary'),
     path('summary/data/uuid:<slug:uuid>', FromDataView.as_view(), name='FormData'),
+    path('summary/users/uuid:<slug:uuid>', FormCompletedUserView.as_view(), name='FormCompletedUser'),
     path('submit/', FormSubmitView.as_view(), name='FormSubmit'),
     path('list/user_id:<user_id>/', FormListView.as_view(), name='FormList'),
 ]
