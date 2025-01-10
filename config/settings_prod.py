5a285febedbf51d3baeebf2732a963e910119be0
Kimmyeonghyeon0 <Kimmyeonghyeom0>
2025-01-10 16:29:07 +0900

    에러 수정

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -5,7 +5,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["43.200.4.153"]
 
-CORS_ALLOW_METHODS = ["GET", "POST"]
+CORS_ALLOW_METHODS = ["GET", "POST", "DELETE", "OPTIONS"]
 
 # # 보안 설정 추가
 # SECURE_HSTS_SECONDS = 3600
@@ -13,6 +13,14 @@ CORS_ALLOW_METHODS = ["GET", "POST"]
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
 
+CORS_ALLOWED_ORIGINS = [
+    # 3000으로 개발환경 세팅
+    "http://localhost:3000",
+    # 배포시
+    "https://fe-three-omega.vercel.app"
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 LOGGING = {
     'version': 1,
