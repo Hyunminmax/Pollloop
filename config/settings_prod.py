f1ea31db1838f959f2187f6992283d0353546f15
Hyunminmax <hyunmin.shin81@gmail.com>
2025-01-14 19:03:51 +0900

    chore/SSL1 - settings_pord.py: FE의 도메인과 연결

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -4,7 +4,7 @@ from .settings_base import *
 DEBUG = False
 
 # 실제 서비스에서는 수정필요 "*" 삭제
-ALLOWED_HOSTS = ["43.200.4.153","*"]
+ALLOWED_HOSTS = ["api.pollloop.store","*"]
 
 # 실제 서비스에서는 로컬삭제, 5174포트는 방법을 찾아야.
 CORS_ALLOWED_ORIGINS = [
@@ -12,19 +12,22 @@ CORS_ALLOWED_ORIGINS = [
     "http://43.200.4.153",    # 백엔드 도메인
     # 배포시
     # "https://fe-three-omega.vercel.app"
+    "https://pollloop.vercel.app",
 ]
 # 실제 서비스에서 필요한지 테스트 필요
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_METHODS = ["GET", "POST", "DELETE", "OPTIONS"]
+CORS_ALLOW_HEADERS = ["content-type", "authorization", "x-csrf-token"]
 
 
 # # 보안 설정 추가
-# SECURE_HSTS_SECONDS = 3600
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-
+SECURE_HSTS_SECONDS = 3600
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 
 LOGGING = {
