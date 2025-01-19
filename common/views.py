f897c4b69894518341348b29e8b10758a8b36ed7
Hyunminmax <hyunmin.shin81@gmail.com>
2025-01-19 23:23:59 +0900

    이메일 비동기 대량발송 개발을 위한 사전준비

--- common/views.py
+++ common/views.py
@@ -3,10 +3,14 @@ import boto3
 from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 from drf_spectacular.utils import extend_schema, OpenApiResponse
+from form.views import FormInvitedView
+from user.views import UserLoginView, UserRegistrationView
 from .serializers import InputFileSerializer
 from rest_framework import status
 from dotenv import dotenv_values
+from rest_framework.test import APIRequestFactory
 
 def upload_file(s3_client, bucket_name, region_name, input_source, S3_key, file):
     # 버킷에 해당 폴더가 있는지 확인
@@ -127,3 +131,65 @@ Multipart/form-data의 경우 예제 선택이 되지 않음. \n
         else:
             print(serializer.errors)  # 검증 실패 시 에러 출력
             return Response(serializer.errors, status=400)            
+        
+class testRegistration(APIView):
+    permission_classes = [AllowAny]  # 누구나 접근 가능
+    authentication_classes = []  # 인증 불필요
+
+    @extend_schema(
+        summary="메일링 테스트용 API 사용 금지!"
+    )
+
+    def post(self, request):
+        user_regstration_view = UserRegistrationView.as_view()
+        factory = APIRequestFactory()
+
+        responses = []
+        for i in range(0, 50):
+            request_data = {
+                    "username": ('mailtestuser'+str(i)), 
+                    "email": ('mailtestuser'+str(i))+'@'+'mail.com', 
+                    "password":'Pass123!', 
+                    "password2": 'Pass123!'
+            }
+            fake_request = factory.post('/user/register/', data=request_data, format='json')
+
+            response = user_regstration_view(fake_request)
+            responses.append(response.data)
+        return Response(responses, status=status.HTTP_201_CREATED)
+
+class testInvited(APIView):
+    permission_classes = [AllowAny]  # 누구나 접근 가능
+    authentication_classes = []  # 인증 불필요
+
+    @extend_schema(
+        summary="메일링 테스트용 API 사용 금지!"
+    )
+    def post(self, request):
+        responses = []
+        for i in range(0, 48):
+            email = 'mailtestuser'+str(i)+'@mail.com'
+            login_data = {
+                'email': email, 
+                'password': 'Pass123!'
+            }
+            
+            factory = APIRequestFactory()
+            user_login_view = UserLoginView.as_view()
+            login_request = factory.post('/user/login', data=login_data, format='json')
+            login_response = user_login_view(login_request)
+
+            access_token = login_response.data.get('access')
+            
+            form_data = {
+                'uuid': '3cc680d5fd654ef29ed282f0a21716d5'
+            }
+            form_invited = FormInvitedView.as_view()
+            form_request = factory.post('/form/invited/', data=form_data, format='json', HTTP_AUTHORIZATION=f'Bearer {access_token}')
+            
+            invited_response = form_invited(form_request)
+            responses.append({
+                'login_response': login_response.data,
+                'invited_response': invited_response.data
+            })
+        return Response(responses, status=status.HTTP_200_OK)
